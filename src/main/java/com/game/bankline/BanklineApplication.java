6ad1fd435683ea783bcf2df3299f1372141f84ca
yukimochii <tanakavv15@gmail.com>
2021-02-27 11:56:33 -0300

    Commit inicial

---
+++ src/main/java/com/game/bankline/BanklineApplication.java
@@ -0,0 +1,13 @@
+package com.game.bankline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/bankline/BanklineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
