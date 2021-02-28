6961b2200844699293f5fbdc79d6897c11c44423
Murilo S. Rodrigues Jr. <murilo.cc.uepb@gmail.com>
2021-02-28 02:07:20 -0300

    Adição de métodos no controller

--- src/main/java/com/game/bankline/controller/LancamentoController.java
+++ src/main/java/com/game/bankline/controller/LancamentoController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.game.bankline.entity.Lancamento;
+import com.game.bankline.entity.PlanoConta;
 import com.game.bankline.repository.LancamentoRepository;
+import com.game.bankline.repository.PlanoContaRepository;
 
 @RestController
 @RequestMapping(value="/lancamentos")
@@ -18,13 +22,21 @@ public class LancamentoController {
 	@Autowired
 	LancamentoRepository lancamentoRepository;
 	
+	@Autowired
+	PlanoContaRepository planoContaRepository;
+	
 	@GetMapping()
 	public List<Lancamento> listarLancamentos(){
 		return  lancamentoRepository.findAll();
 	}
 	
-	@PostMapping
-	public Lancamento salvarLancamento(@RequestBody Lancamento lancamento) {
-		return lancamentoRepository.save(lancamento);
+	@PostMapping("/planos-conta")
+	public PlanoConta salvarPlanoConta(@RequestBody PlanoConta planoConta) {
+		return planoContaRepository.save(planoConta);
+	}
+	
+	@GetMapping("/planos-conta/{login}") 
+	public List<PlanoConta> listarPlanosConta(@PathVariable(value="id") String login ){
+		return planoContaRepository.findAll();
 	}
 }
